2faf80824b51e1d785c2a176f52fb070101d3a02
rakeshrauniyar12 <rakeshrauniyara1234@gmail.com>
2024-11-04 12:15:16 +0530

    header section fix

--- src/Component/ContactForm.js
+++ src/Component/ContactForm.js
@@ -1,4 +1,4 @@
-import {React,useState} from "react";
+import {React,useState,useEffect} from "react";
 import "../Style/Cf.css"; // Assuming you saved the CSS as Cf.css
 import { MdOutlineAttachFile } from "react-icons/md";
 import Service0 from "../Assets/ServiceImage/download 6.png";
@@ -122,6 +122,21 @@ const ContactForm = () => {
     Service0, Service1, Service2, Service3, Service4,
     Service5, Service6, Service7, Service8, Service9
   ];
+  const slides = [
+    { src: Service0, src1: Service1, src2: Service2 },
+    { src: Service3, src1: Service4, src2: Service5 },
+    { src: Service6, src1: Service7, src2: Service8 }
+];
+
+const [currentSlide, setCurrentSlide] = useState(0);
+
+useEffect(() => {
+    const interval = setInterval(() => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    }, 3000); // 3 seconds interval
+
+    return () => clearInterval(interval);
+}, [slides.length]);
   const isMobile = window.innerWidth <= 768;
   return (
     <>
@@ -313,7 +328,35 @@ const ContactForm = () => {
         {
           isMobile?
             <div className="second-div22">
-          <PremiumSection/>
+          <div className="carousel-container1">
+              <h1 className="about-h1" style={{fontSize:"18px",marginTop:"45px"}}>
+                 A Premium Service Brands for Multibrands 
+                 </h1>
+            <div
+                className="carousel-slides1"
+                style={{ transform: `translateX(${-currentSlide * 100}%)` }}
+            >
+                {slides.map((slide, index) => (
+                    <div key={index} className="carousel-item1">
+                        <img
+                            src={slide.src}
+                            alt={`Slide ${index + 1} Image 1`}
+                            className="carousel-image1"
+                        />
+                        <img
+                            src={slide.src1}
+                            alt={`Slide ${index + 1} Image 2`}
+                            className="carousel-image1"
+                        />
+                        <img
+                            src={slide.src2}
+                            alt={`Slide ${index + 1} Image 3`}
+                            className="carousel-image1"
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
           </div>
             :
           <div className="second-div">
