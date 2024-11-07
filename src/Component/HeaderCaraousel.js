aad4f543ec13ea0fc2af7f9949fad3630ad76665
mdShahbazUddin1 <jackayron5@gmail.com>
2024-11-07 17:18:10 +0530

    done

--- src/Component/HeaderCaraousel.js
+++ src/Component/HeaderCaraousel.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import '../Style/HeaderCara.css'; // Import the CSS for styling
+import React, { useState, useEffect } from "react";
+import "../Style/HeaderCara.css"; // Import the CSS for styling
 import Image1 from "../Assets/HeaderCaraousel/image1.png";
 import Image2 from "../Assets/HeaderCaraousel/image2.png";
 import Image3 from "../Assets/HeaderCaraousel/image3.png";
@@ -9,20 +9,45 @@ import Image6 from "../Assets/HeaderCaraousel/image6.png";
 import Image7 from "../Assets/HeaderCaraousel/image7.png";
 import Image8 from "../Assets/HeaderCaraousel/image8.png";
 
-
-
-
 const Carousel = () => {
   // Array of objects holding image paths and names
   const slides = [
-    { src: Image1, name: "Blower Motor", key:"Efficient Climate Control for a Comfortable Ride"},
-    { src: Image2, name: "Alternator", key:"Reliable Power for Your Vehicle"},
-    { src: Image3, name: "Starter Motor", key:"Reliable Engine Ignition for Every Drive" },
-    { src: Image4, name: "Air Suspension Compressor", key:"Smooth Rides with Precision Control" },
-    { src: Image5, name: "EPS Motor", key:"Precision Control for Effortless Steering" },
-    { src: Image6, name: "Liquid Cooled Alternator", key:"High Efficiency for High-Performance Vehicle" },
-    { src: Image7, name: "Wiper Motor", key:"Clear Visibility, No Matter the Weather" },
-    { src: Image8, name: "AC Compressor", key:"Keeping Your Vehicle Cool and Comfortable"},
+    {
+      src: Image1,
+      name: "Blower Motor",
+      key: "Efficient Climate Control for a Comfortable Ride",
+    },
+    { src: Image2, name: "Alternator", key: "Reliable Power for Your Vehicle" },
+    {
+      src: Image3,
+      name: "Starter Motor",
+      key: "Reliable Engine Ignition for Every Drive",
+    },
+    {
+      src: Image4,
+      name: "Air Suspension Compressor",
+      key: "Smooth Rides with Precision Control",
+    },
+    {
+      src: Image5,
+      name: "EPS Motor",
+      key: "Precision Control for Effortless Steering",
+    },
+    {
+      src: Image6,
+      name: "Liquid Cooled Alternator",
+      key: "High Efficiency for High-Performance Vehicle",
+    },
+    {
+      src: Image7,
+      name: "Wiper Motor",
+      key: "Clear Visibility, No Matter the Weather",
+    },
+    {
+      src: Image8,
+      name: "AC Compressor",
+      key: "Keeping Your Vehicle Cool and Comfortable",
+    },
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -49,8 +74,8 @@ const Carousel = () => {
               className="carousel-image112"
             />
             <div className="carousel-caption112">
-            <h1 >{slide.name}</h1>
-            <h3>{slide.key}</h3>
+              <h1>{slide.name}</h1>
+              {/* <h3>{slide.key}</h3> */}
             </div>
           </div>
         ))}
